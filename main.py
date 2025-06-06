617f624b1ea634610afd7f842aa4a49ae8ab00e3
sampenno <48610839+sampenno@users.noreply.github.com>
2025-06-06 10:23:07 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# This script will import a markbook and allow you to estimate a mark
+# Written by Samantha Pennington -6/06/2025
+# Version 0.4
+import numpy
+import csv
+from sklearn.linear_model import LinearRegression
+
+
